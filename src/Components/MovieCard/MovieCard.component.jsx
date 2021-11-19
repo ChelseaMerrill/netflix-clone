9bc18522980cd7cd0faa4bf237f63eee1df3f6d6
Chelsea Merrill <chelmer1223@aol.com>
2021-11-19 11:28:25 -0600

    fix: view movie button

--- src/Components/MovieCard/MovieCard.component.jsx
+++ src/Components/MovieCard/MovieCard.component.jsx
@@ -1,12 +1,15 @@
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
+import './movieCard.styles.css'
 
 export default function MovieCard(props){
     return ( 
         <div key={props.movie.id} className='Block'> 
             <img src={`https://image.tmdb.org/t/p/w400${props.movie.backdrop_path}`}></img>
-            <h5>{props.movie.title}</h5>
-            {/* <p>{props.movie.overview}</p> */}
-            <Link to={{pathname:`singleMovie/${props.movie.id}`, state: props.movie}}><button>View Movie</button></Link>
+            <div className='singleCardInfo'>
+                <h5>{props.movie.title}</h5>
+                {/* <p>{props.movie.overview}</p> */}
+                <Link to={{pathname:`singleMovie/${props.movie.id}`, state: props.movie}}><button>View Movie</button></Link>
+            </div>
         </div>)
 }
 
